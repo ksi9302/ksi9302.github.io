3373e83ce587b0e82596aea16ffeda4aae88885a
Peter Sugin Kim <ksi9302@gmail.com>
2024-03-22 10:42:47 +1100

    switched to aws s3

--- src/utils/getS3File.ts
+++ src/utils/getS3File.ts
@@ -1,15 +1,23 @@
 import AWS from "aws-sdk";
+import S3 from "aws-sdk/clients/s3"; // Import only the S3 client
 
-const s3 = new AWS.S3({
-  endpoint: "p5i6.sg.idrivee2-32.com", //your storage-endpoint
-  accessKeyId: "OsvjKfvCITpheQPNT6UH", //your access-key
-  secretAccessKey: "YerdORDvfhVlj9quspwh4d9Bl1wsNMy2oGNworuD", //your secret-key
+const accessKeyId = process.env.REACT_APP_ACCESS_KEY;
+const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
+
+AWS.config.update({
+  accessKeyId,
+  secretAccessKey,
+});
+
+const s3 = new S3({
+  params: { Bucket: "githubksi" },
+  region: "ap-southeast-2",
 });
 
 const getS3File = async (Key: string) => {
   try {
     const url = await s3.getSignedUrlPromise("getObject", {
-      Bucket: "github",
+      Bucket: "githubksi",
       Key,
     });
 
